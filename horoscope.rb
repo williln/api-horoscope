e0ad80b2d2fc5c33476381b681cbb0781b98e91b
Lacey Williams Henschel <lacey.williams@austin.utexas.edu>
2017-04-15 16:42:55 -0700

    Huzzah! I finished this tutorial

--- horoscope.rb
+++ horoscope.rb
@@ -5,6 +5,16 @@ require 'oauth'
 require 'sinatra'
 
 get '/' do
-  @prediction = ['🍰', '💔', '💪', '🏆', '🐷', '🍕', '🤖'].sample
+  random_wiki = HTTParty.get('https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0')
+  @prediction = random_wiki['query']['random'][0]['title']
+  @id = random_wiki['query']['random'][0]['id']
   erb :index
+end
+
+post '/status' do
+  consumer = OAuth::Consumer.new(ENV['API_KEY'], ENV['API_SECRET'], { site: "https://api.twitter.com", scheme: 'header' })
+  token_hash = { oauth_token: ENV['ACCESS_TOKEN'], oauth_token_secret: ENV['ACCESS_TOKEN_SECRET'] }
+  request_token  = OAuth::AccessToken.from_hash(consumer, token_hash)
+  status = request_token.request(:post, "https://api.twitter.com/1.1/statuses/update.json", status: params[:status])
+  redirect to('/')
 end
